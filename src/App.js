f484ed0da7d47e81345b52db715c8fe3de54d57c
김가경 <gagyeong@gimgagyeong-ui-MacBookAir.local>
2022-08-23 18:05:44 +0900

    앞으로 해야할 파일 구조 잡기

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
-import logo from "./logo.svg";
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import Signup from "./login/Signup";
+import Signup from "./signup/Signup";
 import Naver from "./login/Naver";
 import RedirectURI from "./login/RedirectUrl";
+import Notification from "./notification/Notification";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
+        {/* 소셜로그인 */}
         <Route path="/" exact element={<Naver />} />
         <Route path="/redirectUrl" exact element={<RedirectURI />} />
+        {/* 번호인증 */}
         <Route path="/signUp" exact element={<Signup />} />
+        {/* 알림 API*/}
+        <Route path="/notification" exact element={<Notification />} />
       </Routes>
     </BrowserRouter>
   );
